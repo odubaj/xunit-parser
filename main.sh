ca01f2f8ebd2792b08ac7da9fc67b4d20ffbe3dd
Ondrej Dubaj <odubaj@redhat.com>
2020-11-18 15:00:50 +0100

    resolve project according to component

--- main.sh
+++ main.sh
@@ -9,6 +9,53 @@
 #containery su nejake divne co sa tyka nvr a build_id
 
 # get UI authentification token
+
+#core-services-db team components
+core_services_db=("classloader-leak-test-framework gd gdbm libnsl2 nss_nis rdate yp-tools ypbind ypserv
+                  dejagnu gc less libatomic_ops libconfig libdb mutt galera Judy mariadb mariadb-connector-c 
+                  mariadb-connector-odbc mecab mecab-ipadic multilib-rpm-config mysql python-PyMySQL 
+                  java-comment-preprocessor jna mariadb-java-client postgresql-jdbc properties-maven-plugin 
+                  postgresql-jdbc properties-maven-plugin replacer transfig unixODBC argparse-manpage 
+                  autoconf autoconf213 automake CUnit libpq libstemmer libtool ongres-scram postgresql 
+                  postgresql-odbc pgaudit postgres-decoderbufs python-markdown python-psycopg2 python-pymongo 
+                  scons tokyocabinet cpio libarchive libzip ncompress sqlite star tar xdelta xz zlib 
+                  cronie at crontabs libEMF lockdev pstoedit psutils recode")
+
+#python-maint team components
+python_maint=("babel Cython libyaml pyparsing pytest python-attrs python-backports python-backports-ssl_match_hostname 
+              python-chardet python-click python-configobj python-coverage python-cryptography-vectors python-dateutil 
+              python-decorator python-distutils-extra python-dns python-docutils python-ethtool python-funcsigs 
+              python-html5lib python-hypothesis python-imagesize python-iniparse python-iso8601 python-jinja2 python-lxml 
+              python-mako python-markupsafe python-mock python-nose python-packaging python-pillow python-pip python-pluggy 
+              python-pretend python-py python-pygments python-pysocks python-pytest-mock python-requests python-rpm-generators 
+              python-rpm-macros python-scour python-setuptools python-setuptools_scm python-six python-slip python-snowballstemmer 
+              python-sphinx python-sphinx_rtd_theme python-sphinx-theme-alabaster python-sphinxcontrib-websupport python-sqlalchemy 
+              python-sure python-systemd python-unittest2 python-urllib3 python-virtualenv python-webencodings python-wheel 
+              python-whoosh python2 python2-pip python2-rpm-macros python2-setuptools python3 python36 python38 pytz PyYAML")
+
+#ruby team components
+ruby=("aspell aspell-en compat-libtiff3 dblatex execstack exempi gdisk giflib groff hunspell-ar jbig2dec jbigkit 
+      libcgroup libjpeg-turbo libmng libpipeline libpng libpng12 libpng15 libtiff mailx s-nail man-db man-pages 
+      man-pages-overrides numpy openblas openblas-srpm-macros openjpeg2 qhull scipy git mercurial ruby rubygem-abrt 
+      rubygem-bson rubygem-bundler rubygem-coderay rubygem-diff-lcs rubygem-kramdown rubygem-mongo rubygem-mysql2 
+      rubygem-pg rubygem-rspec rubygem-rspec-core rubygem-rspec-expectations rubygem-rspec-its rubygem-rspec-mocks 
+      rubygem-rspec-support rubygem-thread_order python-distro libuv nodejs nodejs-nodemon nodejs-packaging 
+      uglify-js web-assets")
+
+function get_project() {
+  local component=$1
+
+  if [[ " ${core_services_db[@]} " =~ " ${component} " ]]; then
+    echo "core-services-db"
+  elif [[ " ${python_maint[@]} " =~ " ${component} " ]]; then
+    echo "python-maint"
+  elif [[ " ${ruby[@]} " =~ " ${component} " ]]; then
+    echo "ruby"
+  else
+    echo "arch"
+  fi
+}
+
 function get_ui_token() {
   local username=$1
   local password=$2
@@ -57,8 +104,7 @@ function import_xunit() {
 
 USERNAME=$1
 PASSWORD=$2
-PROJECT=$3
-FILE=$4
+FILE=$3
 
 RP_URL="http://localhost:8080" #"http://reportportal.infrastructure.testing-farm.io"
 TMP_FILE="output.xml"
@@ -67,6 +113,7 @@ TASKINFO_FILE="taskinfo.txt"
 # get data from TestingFarm Xunit
 TASK_ID=$(grep "property name=\"baseosci.artifact-id\" value=" ${FILE} | cut -d'"' -f4)
 ZIP_NAME=$(grep "BASEOS_CI_COMPONENT=" ${FILE} | head -n 1 | cut -d'"' -f2)
+PROJECT=$(get_project ${ZIP_NAME})
 
 # get data about the task from brew
 brew taskinfo -v $TASK_ID > $TASKINFO_FILE
@@ -99,6 +146,8 @@ zip -r $ZIP_FILE $TMP_FILE
 UI_TOKEN=$(get_ui_token ${USERNAME} ${PASSWORD})
 
 API_TOKEN=$(get_api_token ${UI_TOKEN})
+
+echo $PROJECT_NEW
 
 IMPORT=$(import_xunit ${PROJECT} ${API_TOKEN} ${ZIP_FILE})
 echo $IMPORT
