ee032cbed169fd00d21e5b3fdb5d8031fe777011
Ondrej Dubaj <odubaj@redhat.com>
2020-11-24 11:10:17 +0100

    rename wrapper and add comments

--- reportportal-import-results.sh
+++ reportportal-import-results.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# If we're running on the staging Jenkins, don't bother with cold store.
+# Script which downloads import and parser scripts, executes them and creates
+# new XUnit reportportal-results.xml
+
+# If we're not running on the staging Jenkins, do nothing.
 if [ "$JENKINS_MASTER_URL" != "https://baseos-stg-jenkins.rhev-ci-vms.eng.rdu2.redhat.com" ]; then
     exit 1
 fi
